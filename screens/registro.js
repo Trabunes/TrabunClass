a1a266607c6b7a7414388cd7e1b9a4a4748a96af
sr-rccn <49087689+sr-rccn@users.noreply.github.com>
2019-12-12 00:28:23 -0300

    Validación solo Letras

--- screens/registro.js
+++ screens/registro.js
@@ -56,15 +56,14 @@ export default class registro extends React.Component{
             contraseña_tutor: this.state.contraseña_tutor,
           })
         })
-        //   .then((response) => response.json())
-        //   .then((res) => {
-        //     if(res=="Datos Correctos"){
-        //       alert("Registrado Exitosamente");
-        //     }else{
-        //       alert("Datos Incorrectos");
-        //     }
-        //   })
       }
+
+      soloTexto (text, estado) {
+        console.log(text, estado)
+        this.setState({
+            [estado]: text.replace(/[^A-Za-z]/g, ''),
+        });
+    }
 
     formUsuario(){
         if(this.state.tipo == null){
@@ -77,10 +76,10 @@ export default class registro extends React.Component{
             if(this.state.tipo == 0){
                 return(
                     <ScrollView>
-                        <TextInput mode='outlined' style={styles.input} value={this.state.rut_usuario} onChangeText={rut_usuario => this.setState({rut_usuario})} placeholder="Rut" label="Ingresa tu Rut"/>
-                        <TextInput mode='outlined' style={styles.input} value={this.state.nombre_usuario} onChangeText={nombre_usuario => this.setState({nombre_usuario})} placeholder="Nombre" label="Ingresa tu Nombre"/>
-                        <TextInput mode='outlined' style={styles.input} value={this.state.apellido_usuario} onChangeText={apellido_usuario => this.setState({apellido_usuario})} placeholder="Apellido" label="Ingresa tu Apellido"/>
-                        <TextInput mode='outlined' style={styles.input} value={this.state.contraseña_usuario} onChangeText={contraseña_usuario => this.setState({contraseña_usuario})} placeholder="Contraseña" label="Ingresa tu Contraseña"/>
+                        <TextInput mode='outlined' style={styles.input} value={this.state.rut_usuario} onChangeText={rut_usuario => this.setState({rut_usuario})} placeholder="Rut" label="Ingresa tu Rut" />
+                        <TextInput mode='outlined' style={styles.input} value={this.state.nombre_usuario} onChangeText={nombre_usuario => this.soloTexto(nombre_usuario, 'nombre_usuario')} placeholder="Nombre" label="Ingresa tu Nombre" maxLength={30}/>
+                        <TextInput mode='outlined' style={styles.input} value={this.state.apellido_usuario} onChangeText={apellido_usuario => this.soloTexto(apellido_usuario, 'apellido_usuario')} placeholder="Apellido" label="Ingresa tu Apellido" maxLength={30}/>
+                        <TextInput mode='outlined' style={styles.input} value={this.state.contraseña_usuario} onChangeText={contraseña_usuario => this.setState({contraseña_usuario})} placeholder="Contraseña" label="Ingresa tu Contraseña" maxLength={15}/>
                         <Button mode='outlined' style={styles.registrate} onPress={this.validarUsuario.bind(this)} >
                                 <Text style={styles.texto} >Registrate</Text>
                         </Button>
@@ -91,8 +90,8 @@ export default class registro extends React.Component{
                 return(
                     <ScrollView>
                         <TextInput mode='outlined' style={styles.input} value={this.state.rut_tutor} onChangeText={rut_tutor=> this.setState({rut_tutor})} placeholder="Rut" label="Ingresa tu Rut"/>
-                        <TextInput mode='outlined' style={styles.input} value={this.state.nombre_tutor} onChangeText={nombre_tutor => this.setState({nombre_tutor})} placeholder="Nombre" label="Ingresa tu Nombre"/>
-                        <TextInput mode='outlined' style={styles.input} value={this.state.apellido_tutor} onChangeText={apellido_tutor => this.setState({apellido_tutor})} placeholder="Apellido" label="Ingresa tu Apellido"/>
+                        <TextInput mode='outlined' style={styles.input} value={this.state.nombre_tutor} onChangeText={nombre_tutor => this.soloTexto(nombre_tutor, 'nombre_tutor')} placeholder="Nombre" label="Ingresa tu Nombre"/>
+                        <TextInput mode='outlined' style={styles.input} value={this.state.apellido_tutor} onChangeText={apellido_tutor => this.soloTexto(apellido_tutor, 'apellido_tutor')} placeholder="Apellido" label="Ingresa tu Apellido"/>
                         <TextInput mode='outlined' style={styles.input} value={this.state.contraseña_tutor} onChangeText={contraseña_tutor => this.setState({contraseña_tutor})} placeholder="Contraseña" label="Ingresa tu Contraseña"/>
                         <Button mode='outlined' style={styles.registrate} onPress={this.validarTutor.bind(this)}>
                                 <Text style={styles.texto} >Registrate</Text>
